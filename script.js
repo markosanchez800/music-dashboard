1df1c4a367f1ed49e16bb7626f52fd37e532b9c6
Patrick Ly <patrickly72@gmail.com>
2021-04-07 19:58:58 -0700

    added youtube api functionality

--- script.js
+++ script.js
@@ -12,15 +12,24 @@ fetch('https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&type=video&pa
   })
   .then(function (data) {
     console.log(data);
-    data.items.forEach(item => {
-        videos = `
-
-        <iframe width="420" height="315" src="http://www.youtube.com/embed${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
-
-
-        `
-        musicVideos.append(video)
-    })
+        for(var i =0; i<data.items.length; i++){
+            console.log(data.items[i].id.videoId)
+            function addVideo(){
+                var vidID = data.items[i].id.videoId;
+                var video = document.createElement("iframe");
+                video.setAttribute("width","210");
+                video.setAttribute("height", "158");
+                video.setAttribute("src","http://www.youtube.com/embed/"+vidID )
+                musicVideos.appendChild(video);
+            
+            }
+                addVideo();
+        }
+                
+    // data.items.forEach(item => {
+    //     videos = `<iframe width="420" height="315" src="http://www.youtube.com/embed${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`
+    //     musicVideos.append(video)
+    // })
 
   });
 
