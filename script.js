703f950659f91f1865f1bb7a250d748352600cfb
markosanchez800 <markosanchez800@gmail.com>
2021-04-07 22:54:20 -0700

    site includes youtube videos and fixed some html and java text

--- script.js
+++ script.js
@@ -86,14 +86,55 @@ var getTopTracks = function(id) {
    });
  };
 
-document.getElementById('search-form').addEventListener('submit', function(e) {
+
+
+ document.getElementById('search-form').addEventListener('submit', function(e) {
  e.preventDefault();
  console.log('success!')
  searchArtists(document.getElementById('searchbox').value);
+ getMusicVideos(document.getElementById('searchbox').value);
 }, false);
 
 
+var apiKey= "AIzaSyADNoOQ9XzgB7BUXeHVLhgIFbXlbjVRq5c";
+var maxResults = 10;
+var searchQuery = document.getElementById("searchbox")
+var video = ""
+var musicVideos = document.getElementById("musicvideos")
 
+function getMusicVideos(){
+    $("#musicvideos").empty();
+var artistName = document.getElementById("searchbox").value;
+fetch('https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&type=video&part=snippet&maxResults='+maxResults+'&q='+artistName)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+        for(var i =0; i<data.items.length; i++){
+            console.log(data.items[i].id.videoId)
+            function addVideo(){
+                var vidID = data.items[i].id.videoId;
+                var video = document.createElement("iframe");
+                video.setAttribute("width","210");
+                video.setAttribute("height", "158");
+                video.setAttribute("src","http://www.youtube.com/embed/"+vidID )
+                musicVideos.appendChild(video);
+            
+            }
+                addVideo();
+        }
+                
+    // data.items.forEach(item => {
+    //     videos = `<iframe width="420" height="315" src="http://www.youtube.com/embed${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`
+    //     musicVideos.append(video)
+    // })
+
+  });
+
+}
+
+  //document.getElementById("search-form").addEventListener("click",getMusicVideos);
 
 
 // musix key 0d49953ffed1270bd1dd131b139e95d1
