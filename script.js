692b78cd49d181e22e0987e7fb43c0ed24bf4b91
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-04-09 03:23:19 -0400

    added lyricbox as an Id for the element to be found.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ trackSeven = document.getElementById('trackSeven')
 trackEight = document.getElementById('trackEight')
 trackNine = document.getElementById('trackNine')
 trackTen = document.getElementById('trackTen')
+lyricbox = document.getElementById('lyrics')
  
   clientID = '9577ec53580a46c686cbb0729d57118e';
   clientSecret = '903925af8da34bbabffe55187620ca4b';
@@ -56,7 +57,7 @@ var searchArtists = function(query) {
      type: 'artist'
    },
    headers: {
-       "Authorization": "Bearer " + "BQC6wZf3OTPanpreZ0o7xiYkHEATe34i4krQetGZYpAf5TNZtsA7ld6r0i8omch1ku9PA_-PCTmfzmhqH0OQeGOe4QQCwgmLu0_ZJBAqQcdNyTHfjKMh-BLJwtSUky5SBJDIo0SMb9lzTLXAAm04SYU"
+       "Authorization": "Bearer " + "BQDgUPMud17zaKRxgYQvvk84xBNBtxbJnpUOQJ_B_6NZxSXq-_EgHDHXiCCfh6itJPsMfnZ4-VN4BLffvgV3NGtqqvXZtbaOM8BKPd3eFHNeLpQjBEhI3KhJBlbMgS_s03-MIbwWRRdmzrcx9E9eB8I"
     },
    success: function(response) {
        tempPic = response.artists.items[0].images[0].url;
@@ -75,7 +76,7 @@ var getTopTracks = function(id) {
    $.ajax({
      url: 'https://api.spotify.com/v1/artists/' + id + '/top-tracks?market=US',
      headers: {
-         "Authorization": "Bearer " + "BQC6wZf3OTPanpreZ0o7xiYkHEATe34i4krQetGZYpAf5TNZtsA7ld6r0i8omch1ku9PA_-PCTmfzmhqH0OQeGOe4QQCwgmLu0_ZJBAqQcdNyTHfjKMh-BLJwtSUky5SBJDIo0SMb9lzTLXAAm04SYU         "
+         "Authorization": "Bearer " + "BQDgUPMud17zaKRxgYQvvk84xBNBtxbJnpUOQJ_B_6NZxSXq-_EgHDHXiCCfh6itJPsMfnZ4-VN4BLffvgV3NGtqqvXZtbaOM8BKPd3eFHNeLpQjBEhI3KhJBlbMgS_s03-MIbwWRRdmzrcx9E9eB8I "
     },
      success: function(response) {
          trackOne.innerHTML = response.tracks[0].name;
@@ -89,32 +90,25 @@ var getTopTracks = function(id) {
          trackNine.innerHTML = response.tracks[8].name;
          trackTen.innerHTML = response.tracks[9].name;
          getLyrics(response.tracks[0].name);
-         searchLyrics(response.tracks[0].name);
        console.log(response);
      }
    });
  };
 
- var getLyrics = function(arg){
-     $.ajax({
-         url: 'https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=' + arg + '&quorum_factor=1&page_size=1&apikey=0d49953ffed1270bd1dd131b139e95d1',
-         success: function(response) {
-             console.log(response);
-             
-            //getRealLyrics(response.)
-         }
-     })
- }
 
- var searchLyrics = function(arg){
+ var getLyrics = function(query,arg){
     $.ajax({
-        url:'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=',
+        url: "https://api.lyrics.ovh/v1/" + query + "/" + arg,
         success: function(response) {
             console.log(response);
-            
+            lyricStuff = response.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+            lyricBOx.innerHTML=lyricStuff;
+
+           //getRealLyrics(response.)
         }
     })
 }
+
 
 
  document.getElementById('search-form').addEventListener('submit', function(e) {
