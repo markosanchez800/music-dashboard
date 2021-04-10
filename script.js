dba4f9de71390e590c5b337ecbd38a5c67f44ca9
Patrick Ly <patrickly72@gmail.com>
2021-04-09 21:22:29 -0700

    added localstorage button function

--- script.js
+++ script.js
@@ -1,17 +1,203 @@
-var apiKey= "AIzaSyADNoOQ9XzgB7BUXeHVLhgIFbXlbjVRq5c";
+artist = document.getElementById('artistname'),
+monthlyListen = document.getElementById('monthlyNumber'),
+artistInfo = document.getElementById('artistInfo'),
+trackOne = document.getElementById('trackOne'),
+trackTwo = document.getElementById('trackTwo'),
+trackThree = document.getElementById('trackThree'),
+trackFour = document.getElementById('trackFour'),
+trackFive = document.getElementById('trackFive'),
+trackSix = document.getElementById('trackSix'),
+trackSeven = document.getElementById('trackSeven'),
+trackEight = document.getElementById('trackEight'),
+trackNine = document.getElementById('trackNine'),
+trackTen = document.getElementById('trackTen'),
+searchArea = document.getElementById('searchHistoree'),
+lyricBox = document.getElementById('lyrics')
+var artists;
+var searchHistory = localStorage.getItem("artists");
+if(searchHistory === null){
+  artists = [];
+}
+
+else{artists = JSON.parse( searchHistory );}
+
+function addButtons(){
+  var buttonText = document.createTextNode(artistName);
+  var form = document.getElementById("search-form");
+  var button = document.createElement("button");
+  button.appendChild(buttonText);
+  form.appendChild(button);}
+
+function createButtons(){
+  for(var i =0; i<artists.length; i++){
+      function addButton(){
+          var buttonText = document.createTextNode(artists[i]);
+          var form = document.getElementById("search-form");
+          var button = document.createElement("button");
+          button.appendChild(buttonText);
+          button.setAttribute("value",artists[i])
+          button.setAttribute("onclick", "event.preventDefault()")
+          form.appendChild(button);
+      
+      }
+          addButton();
+  }
+          }
+ 
+  clientID = '9577ec53580a46c686cbb0729d57118e';
+  clientSecret = '903925af8da34bbabffe55187620ca4b';
+
+  keyGen = function(){
+      tempPass = '';
+      charLim = 57;
+    possibleKeys= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","0","1","2","3","4","5","6","7","8","9","_",".","-","~"];
+    for ( i=0; i < charLim; i++){
+        tempPass += possibleKeys[Math.floor(Math.random()*possibleKeys.length)];
+      }
+      console.log(tempPass);
+      return tempPass;
+      
+  }();
+
+  _getToken = function(){
+
+    result = fetch('https://accounts.spotify.com/api/token', {
+       method: 'POST',
+       headers: {
+           'Content-Type' : 'application/x-www-form-urlencoded', 
+           'Authorization' : 'Basic ' + btoa(clientID + ':' + clientSecret)
+       },
+       body: 'grant_type=client_credentials'
+   });
+
+
+    data = result;
+    console.log(data);
+   return data.access_token;
+   
+   
+
+}();
+
+
+var searchArtists = function(query) {
+ $.ajax({
+   url: 'https://api.spotify.com/v1/search',
+   data: {
+     q: query,
+     type: 'artist'
+   },
+   headers: {
+       "Authorization": "Bearer " + "BQBBXZVdhXKzIY6cxWC2MZ2SHiJSLv-beKK2UPG113mTY_-cPT9_u5MJ29Viud33IsH-p7ql_GyU_Goud1E2do8AzmhFWzLaMMf23D3Y_cweAiib3w1UJBjy669BvEnAkAiAQjQoaeFvM8XRisikiGA"
+   },
+   
+   success: function(response) {
+       tempPic = response.artists.items[0].images[0].url;
+       artistInfo.setAttribute("style","background-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(255,0,0,0)), url(" + tempPic + ")");
+       artist.innerHTML = JSON.stringify(response.artists.items[0].name);
+       monthlyListen.innerHTML = JSON.stringify(response.artists.items[0].followers.total);
+       id = response.artists.items[0].id;
+       getTopTracks(query,id);
+     console.log(response);
+   }
+ });
+ 
+};
+
+var getTopTracks = function(query,id) {
+   $.ajax({
+     url: 'https://api.spotify.com/v1/artists/' + id + '/top-tracks?market=US',
+     headers: {
+         "Authorization": "Bearer " + "BQBBXZVdhXKzIY6cxWC2MZ2SHiJSLv-beKK2UPG113mTY_-cPT9_u5MJ29Viud33IsH-p7ql_GyU_Goud1E2do8AzmhFWzLaMMf23D3Y_cweAiib3w1UJBjy669BvEnAkAiAQjQoaeFvM8XRisikiGA"
+     },
+     success: function(response) {
+         trackOne.innerHTML = response.tracks[0].name;
+         trackOne.addEventListener("click", function(){
+         getLyrics(query,response.tracks[0].name);
+         })
+         trackTwo.innerHTML = response.tracks[1].name;
+         trackTwo.addEventListener("click", function(){
+          getLyrics(query,response.tracks[1].name);
+          })
+         trackThree.innerHTML = response.tracks[2].name;
+         trackThree.addEventListener("click", function(){
+          getLyrics(query,response.tracks[2].name);
+          })
+         trackFour.innerHTML = response.tracks[3].name;
+         trackFour.addEventListener("click", function(){
+          getLyrics(query,response.tracks[3].name);
+          })
+         trackFive.innerHTML = response.tracks[4].name;
+         trackFive.addEventListener("click", function(){
+          getLyrics(query,response.tracks[4].name);
+          })
+         trackSix.innerHTML = response.tracks[5].name;
+         trackSix.addEventListener("click", function(){
+          getLyrics(query,response.tracks[5].name);
+          })
+         trackSeven.innerHTML = response.tracks[6].name;
+         trackSeven.addEventListener("click", function(){
+          getLyrics(query,response.tracks[6].name);
+          })
+         trackEight.innerHTML = response.tracks[7].name;
+         trackEight.addEventListener("click", function(){
+          getLyrics(query,response.tracks[7].name);
+          })
+         trackNine.innerHTML = response.tracks[8].name;
+         trackNine.addEventListener("click", function(){
+          getLyrics(query,response.tracks[8].name);
+          })
+         trackTen.innerHTML = response.tracks[9].name;
+         trackTen.addEventListener("click", function(){
+          getLyrics(query,response.tracks[9].name);
+          })
+       console.log(response);
+     }
+   });
+ };
+
+//  var getLyrics = function(query,arg){
+//      $.ajax({
+//          url: "https://api.lyrics.ovh/v1/" + query + "/" + arg,
+//          success: function(response) {
+//              console.log(response);
+//              lyricStuff = response.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+//              lyricBox.innerHTML = lyricStuff;
+//          }
+//      })
+//  }
+
+
+ document.getElementById('search-form').addEventListener('submit', function(e) {
+ e.preventDefault();
+ console.log('success!')
+ searchArtists(document.getElementById('searchbox').value);
+ getMusicVideos(document.getElementById('searchbox').value);
+ addButtons()
+//  searchHistory(document.getElementById('searchbox').value);
+}, false);
+
+// document.getElementById('search-form').addEventListener('submit', function(e) {
+//   searchHistory();
+// })
+
+var apiKey= "AIzaSyAqMcywe4dEC4LFFRqaRmNyIPp3OK7DsMU";
 var maxResults = 10;
 var searchQuery = document.getElementById("searchbox")
 var video = ""
 var musicVideos = document.getElementById("musicvideos")
 
 function getMusicVideos(){
-var artistName = document.getElementById("searchbox").value;
+    $("#musicvideos").empty();
+artistName = document.getElementById("searchbox").value;
 fetch('https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&type=video&part=snippet&maxResults='+maxResults+'&q='+artistName)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+    artists.push(artistName);
+    localStorage.setItem("artists", JSON.stringify( artists ) );
         for(var i =0; i<data.items.length; i++){
             console.log(data.items[i].id.videoId)
             function addVideo(){
@@ -21,21 +207,29 @@ fetch('https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&type=video&pa
                 video.setAttribute("height", "158");
                 video.setAttribute("src","http://www.youtube.com/embed/"+vidID )
                 musicVideos.appendChild(video);
+                
             
             }
                 addVideo();
         }
                 
-    // data.items.forEach(item => {
-    //     videos = `<iframe width="420" height="315" src="http://www.youtube.com/embed${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`
-    //     musicVideos.append(video)
-    // })
 
   });
 
 }
 
-document.getElementById("submit").addEventListener("click", function(event){
-    event.preventDefault()
-  });
-  document.getElementById("submit").addEventListener("click",getMusicVideos);
+createButtons();
+
+// function searchHistory() {
+//     button = document.createElement("button");
+//     button.setAttribute("style","width:100px");
+//     button.innerHTML = artistName.value;
+//     document.querySelectorAll('button')
+//     .forEach(button => {
+//       button.addEventListener('click', function() {
+//         searchArtists(target.textContent);
+//         getMusicVideos(target.textContent);
+//       });
+//     });
+//    }
+//     searchArea.appendChild(button);
